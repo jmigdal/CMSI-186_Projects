62ffa61a9b6ff0f566f4f4ed2fca85614be812c6
jmigdal <35497548+jmigdal@users.noreply.github.com>
2018-01-16 09:54:00 -0800

    Added file header

---
+++ homework00/Hello.java
@@ -0,0 +1,22 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.java
+ *  Purpose       :  Says "Hello world"
+ *  Author        :  James Migdal
+ *  Date          :  2017-01-14
+ *  Description   :  See purpose
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-14  James Migdal  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ 
+public class HelloWorld {
+  public static void main( String args[] ) {
+    System.out.println( "Hello, world" );
+  }
+}
